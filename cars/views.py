27575b13f4bbc5824476e6443317c664a8d8a36e
kleitonmac <kleiton76@yahoo.com>
2025-05-21 01:48:50 -0300

    Mesclando com o remoto

--- cars/views.py
+++ cars/views.py
@@ -2,33 +2,70 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 #como por exemplo essa e uma funcao based view pois esta recebendo valor pelo view da pasta mae app
-class CarsView(View):
+
+
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+     
+     
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+           cars = cars.filter(model__icontains=search)
+        return cars
+         
+'''
+List view tem um metodo de queryset que receber os filtros do campo de busca       
+views orientadas a classes
+função based view  ela utilza a Class import View e receber toda sua herança
     
-    def get(self, request):
-         cars = Car.objects.all().order_by('model')
-         search = request.GET.get('search')
+    Create view viu de criacao de cadastro
+'''
+
+"""
+  @method_decorator(login_required(login_url='login'), name='dispatch')
+  metodo para passar uma camada de autentificacao vai mandar o cliente direto para fazer login 
+"""
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class NewCarCreateView(CreateView):
     
-         if  search:   
-             cars = cars.filter(model__icontains=search)
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
     
-         return render(
-        request, 
-        'cars.html',
-        {'cars': cars}
-        )
-#views orientadas a classes
-#função based view  ela utilza a Class import View e receber toda sua herança
-class NewCarView(View):
     
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', { 'new_car_form': new_car_form })
     
-    def post(self , request):
-        new_car_form = CarModelForm(request.POST , request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+class CarDetailView(DetailView):
+        model = Car
+        template_name = 'car_detail.html'
+        
+       
+@method_decorator(login_required(login_url='login'), name='dispatch')        
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'      
+    
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
+      
+      
+
+
